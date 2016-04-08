4e86652af639b5936bc8aeba8a30ce8a7d5229b8
Antti Viljakainen <antti.viljakainen@otavanopisto.fi>
2016-04-08 22:38:14 +0300

    - Ex 2

---
+++ src/main/java/fi/sondeco/algo2/HashTable.java
@@ -0,0 +1,87 @@
+package fi.sondeco.algo2;
+
+public class HashTable {
+
+  /**
+   * Constructs new HashTable with capacity m.
+   * 
+   * @param m capacity.
+   */
+  public HashTable(int m) {
+    this.table = new int[m];
+  }
+  
+  /**
+   * Adds a key to the table. If the table is full, 
+   * throws a runtime exception.
+   * 
+   * @param key key to be added
+   */
+  public void add(int key) {
+    int h = hash(key);
+    
+    for (int i = 0; i < table.length; i++) {
+      int index = (h + i) % table.length;
+      
+      if (table[index] <= 0) {
+        table[index] = key;
+        return;
+      }
+    }
+    
+    throw new RuntimeException("HashTable is full.");
+  }
+  
+  /**
+   * Removes key from table. Returns true if key was removed, 
+   * false if the key didn't exist.
+   * 
+   * @param key key to be removed.
+   * @return true if operation was successful
+   */
+  public boolean remove(int key) {
+    int h = hash(key);
+    
+    for (int i = 0; i < table.length; i++) {
+      int index = (h + i) % table.length;
+      
+      if (table[index] == key) {
+        table[index] = REMOVED;
+        return true;
+      } else if (table[index] == EMPTY)
+        return false;
+    }
+    
+    return false;
+  }
+  
+  /**
+   * Returns true if the table contains the item.
+   * 
+   * @param key the key
+   * @return true if the key exists in the table, false otherwise
+   */
+  public boolean contains(int key) {
+    int h = hash(key);
+    
+    for (int i = 0; i < table.length; i++) {
+      int index = (h + i) % table.length;
+      
+      if (table[index] == key)
+        return true;
+      else if (table[index] == EMPTY)
+        return false;
+    }
+    
+    return false;
+  }
+  
+  private int hash(int key) {
+    return key % table.length;
+  }
+
+  private static final int EMPTY = 0;
+  private static final int REMOVED = -1;
+  
+  private int[] table;
+}
---
+++ src/test/java/fi/sondeco/algo2/TestHashTable.java
@@ -0,0 +1,48 @@
+package fi.sondeco.algo2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestHashTable {
+
+  @Test
+  public void test() {
+    HashTable ht = new HashTable(10);
+    
+    int[] testdata = new int[] { 21, 55, 31, 49, 52, 72, 26, 19 };
+    
+    // One constant element
+    ht.add(10);
+
+    /**
+     * Test adding of the elements
+     */
+    for (int i = 0; i < testdata.length; i++) {
+      ht.add(testdata[i]);
+      
+      assertTrue(ht.contains(testdata[i]));
+    }
+
+    /**
+     * Test removing of the elements
+     */
+    for (int i = 0; i < testdata.length; i++) {
+      ht.remove(testdata[i]);
+      
+      assertFalse(ht.contains(testdata[i]));
+    }
+    
+    /**
+     * Test successive adding and removing on a used table
+     */
+    for (int i = 0; i < testdata.length; i++) {
+      ht.add(testdata[i]);
+      assertTrue(ht.contains(testdata[i]));
+
+      ht.remove(testdata[i]);
+      assertFalse(ht.contains(testdata[i]));
+    }
+  }
+
+}
